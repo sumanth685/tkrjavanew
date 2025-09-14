eb33a9aba4968541a478a1c311587440441caf65
Sumanth_Sujay <Sumanth_Sujay@192.168.1.49>
2025-09-14 10:32:43 +0530

    operators and type conversion

---
+++ secondproject/src/com/second/se/TypeConversion.java
@@ -0,0 +1,22 @@
+package com.second.se;
+
+public class TypeConversion {
+	public static void main(String args[]) {
+		/*implicit conversion 
+		 * integer to float value conversion
+		 */
+	int a=20;
+	float b=a;
+	System.out.println(b);
+	/*
+	 * Explicit conversion 
+	 * double value to integer value 
+	 */
+	double c=30.5;
+	int d=(int)c;
+	System.out.println(d);
+	
+		
+	}
+
+}
