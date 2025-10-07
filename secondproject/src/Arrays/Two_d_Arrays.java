f5907b744e7398588ce576d9b902c50385fa1867
Sumanth_Sujay <Sumanth_Sujay@192.168.1.46>
2025-10-07 21:51:35 +0530

    Demo about Arrays

---
+++ secondproject/src/Arrays/Two_d_Arrays.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class Two_d_Arrays {
+	
+	    public static void main(String[] args) {
+	        // Define a 2D array
+	        int[][] matrix = { {1, 2, 3},{4, 5, 6},{7, 8, 9}};
+
+	        // Trace the array by printing each element with its indices
+	        for (int i = 0; i < matrix.length; i++) {
+	            for (int j = 0; j < matrix[i].length; j++) {
+	                System.out.println("Element at [" + i + "][" + j + "] = " + matrix[i][j]);
+	            }
+	        }
+	    }
+	}
+
+
