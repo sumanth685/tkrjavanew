8133a6cb8ce3c4128629093d27e1fe0de4648415
Sumanth_Sujay <Sumanth_Sujay@192.168.1.46>
2025-10-07 21:44:51 +0530

    Demo about Inheritance

---
+++ secondproject/src/Inheritance/MultilevelInheritance.java
@@ -0,0 +1,34 @@
+package Inheritance;
+
+
+
+
+	class Animals1 {
+	    void eat() {
+	        System.out.println("I can eat.");
+	    }
+	}
+
+	class Dogs extends Animals1 {
+	    void bark() {
+	        System.out.println("I can bark.");
+	    }
+	}
+
+	class Puppy extends Dogs {
+	    void weep() {
+	        System.out.println("I am a puppy, I can weep.");
+	    }
+	}
+
+	public class MultilevelInheritance {
+	    public static void main(String[] args) {
+	        Puppy p = new Puppy();
+	        p.eat();   
+	        p.bark();  
+	        p.weep();  
+	    }
+	}
+
+
+
